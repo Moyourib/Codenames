d86bddbaf68775353c58fce63a53f76e02a39105
Moyouri Bhattacharjee <moyourib@gmail.com>
2017-11-22 14:02:42 -0500

    manual sign in

--- client/Start.jsx
+++ client/Start.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
+import React from 'react'
 import withAuth from './withAuth'
 import UserHome from './UserHome'
 
 const Start = (props) => {
   return props.user ?
   (
-    <UserHome user={props.user}/>
+    <UserHome user={props.user} />
   )
   :
   (
@@ -16,18 +14,18 @@ const Start = (props) => {
         <div>
           <h6>Sign In</h6>
           <form onSubmit={props.signIn}>
-            <input type="email" name="email"/>
-            <input type="password" name="password"/>
-            <input type="submit"/>
+            <input type="email" name="email" />
+            <input type="password" name="password" />
+            <input type="submit" />
           </form>
         </div>
 
         <div>
           <h6>Sign Up</h6>
           <form onSubmit={props.createUser}>
-            <input type="email" name="email"/>
-            <input type="password" name="password"/>
-            <input type="submit"/>
+            <input type="email" name="email" />
+            <input type="password" name="password" />
+            <input type="submit" />
           </form>
         </div>
 
